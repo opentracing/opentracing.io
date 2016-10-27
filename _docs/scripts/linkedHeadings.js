88ff68ae4b198b4c39d0865fd00382ac2f912cb3
Dustin Lessard <dustin.lessard@workiva.com>
2016-10-27 16:41:28 -0400

    linkedHeadings : included h1 and h4

--- _docs/scripts/linkedHeadings.js
+++ _docs/scripts/linkedHeadings.js
@@ -1,3 +1,3 @@
-$('h2, h3').filter('[id]').each(function () {
+$('h1, h2, h3, h4').filter('[id]').each(function () {
     $(this).html('<a href="#'+$(this).attr('id')+'">' + $(this).text() + '</a>');
 });
