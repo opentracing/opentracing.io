f27b7106e9f548605d0ccc5c1a29925627220a56
Austin Parker <austin@lightstep.com>
2018-09-07 15:33:15 -0400

    fix menu js fix link

--- themes/tracer/assets/js/app.js
+++ themes/tracer/assets/js/app.js
@@ -3,7 +3,10 @@ console.log("Welcome to the OpenTracing docs!");
 // hacky, but this preserves position on sidebar nav through page loads
 // also makes the current page bold
 document.addEventListener("DOMContentLoaded", function(){
-    let currentUrl = "//" + document.location.host + document.location.pathname
+    let currentUrl = document.location.href
+    if (document.location.hostname === "localhost") {
+        currentUrl = "//" + document.location.host + document.location.pathname
+    }
     if (document.querySelectorAll(".submenu").length > 0) {
         var currentPage = document.querySelectorAll("a[href='" + currentUrl + "']")
         currentPage[0].style.fontWeight = 'bold';
